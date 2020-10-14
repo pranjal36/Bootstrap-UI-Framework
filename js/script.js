1f9dcff97e5ea52cbf8d5efbf9f103b81ee4dece
Pranjal <pranjalgupta362@gmail.com>
2020-10-14 18:07:42 +0530

    Tested onchange

---
+++ js/script.js
@@ -0,0 +1,36 @@
+
+$(document).ready(function(){
+    /*$('[data-toggle="tooltip"]').tooltip();*/
+
+    $("#mycarousel").carousel( { interval: 2000 } );
+    $("#carouselButton").click(function(){
+        if ($("#carouselButton").children("span").hasClass('fa-pause')) {
+            $("#mycarousel").carousel('pause');
+            $("#carouselButton").children("span").removeClass('fa-pause');
+            $("#carouselButton").children("span").addClass('fa-play');
+        }
+        else if ($("#carouselButton").children("span").hasClass('fa-play')){
+            $("#mycarousel").carousel('cycle');
+            $("#carouselButton").children("span").removeClass('fa-play');
+            $("#carouselButton").children("span").addClass('fa-pause');                    
+        }
+    });
+
+        $("#buttonLogin").click(function(){
+            $('#loginModal').modal('show');
+        });
+
+        $("#buttonReservation").click(function(){
+            $('#reservationFormModal').modal('show');
+        });
+});
+
+/*$(document).ready(function(){
+    $("#mycarousel").carousel( { interval: 2000 } );
+    $("#carousel-pause").click(function(){
+        $("#mycarousel").carousel('pause');
+    });
+    $("#carousel-play").click(function(){
+        $("#mycarousel").carousel('cycle');
+    });
+});*/
